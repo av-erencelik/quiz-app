4901a11544b4d7987b64672b5b8c9f3e91e36bd6
Mehmet Eren Çelik <erenmemo05@gmail.com>
2022-11-18 19:07:36 +0300

    added hardcoded scores to header

--- src/components/header/Nav.js
+++ src/components/header/Nav.js
@@ -1,14 +1,18 @@
 import Button from "../ui/Button";
+import Score from "./Score";
 
 export default function Nav() {
   return (
-    <ul className="nav">
-      <li className="link">
-        <Button>NEW QUIZ</Button>
-      </li>
-      <li className="link">
-        <Button>RESTART QUIZ</Button>
-      </li>
-    </ul>
+    <div className="nav-container">
+      <ul className="nav">
+        <li className="link">
+          <Button>NEW QUIZ</Button>
+        </li>
+        <li className="link">
+          <Button>RESTART QUIZ</Button>
+        </li>
+      </ul>
+      <Score></Score>
+    </div>
   );
 }
---
+++ src/components/header/Score.js
@@ -0,0 +1,12 @@
+export default function Score() {
+  return (
+    <div className="score-container">
+      <h3 className="score">
+        SCORE: <span> 0</span>
+      </h3>
+      <h3 className="score">
+        BEST SCORE: <span> 0</span>
+      </h3>
+    </div>
+  );
+}
