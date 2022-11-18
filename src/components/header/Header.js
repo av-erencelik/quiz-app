11749d3db6fe725ec3b9502d3c82cf6691377c5e
Mehmet Eren Çelik <erenmemo05@gmail.com>
2022-11-18 18:14:41 +0300

    added quiz icon on header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,11 @@
 import Nav from "./Nav";
-
+import { MdQuiz } from "react-icons/md";
 export default function Header() {
   return (
     <header className="header">
-      <div className="empty"></div>
+      <div className="empty">
+        <MdQuiz></MdQuiz>
+      </div>
       <h1 className="header-title title">TRIVIA QUIZ</h1>
       <Nav></Nav>
     </header>
